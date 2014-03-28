792cc1b84221eeb102b52e6a798ee68d39aa8b42
Anna Petry <annapetry@Annas-MacBook-Pro.local>
2014-03-28 09:17:32 -0700

    Added favicon images.

--- inc/header.php
+++ inc/header.php
@@ -2,12 +2,13 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=uft-8"/>
     <meta name="viewport" content="width=device-width" />
+    <meta name="p:domain_verify" content="bcfb9350c1d6362fac4df0ee560e0d4b"/>
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="/css/normalize.css" type="text/css" media="screen">
     <link rel="stylesheet" href="/css/grid.css" type="text/css" media="screen">
     <link rel="stylesheet" href="/css/style.css" type="text/css" media="screen">
-    <link href="/inc/Inconsolata.pfa" type="text/plain" media="screen">
-    <link href="/inc/ErkicaType_I.ttf" type="text/plain" media="screen">
+    <link rel="icon" href="/img/geo-32.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="/img/geo-32.ico" type="image/ico" />
     <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,100italic' rel='stylesheet' type='text/css'>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type='text/javascript' src="/script.js"></script>
